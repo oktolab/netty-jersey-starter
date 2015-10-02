c7b5bdaefcc5ba8f080cca1458cd436d2549b606
arthur.pdr@gmail.com <arthur.pdr@gmail.com>
2015-10-02 15:43:23 -0300

    hystrix filter initial code

---
+++ src/main/java/br/com/oktolab/netflixoss/nettyrest/provider/filter/HystrixContextFilter.java
@@ -0,0 +1,30 @@
+package br.com.oktolab.netflixoss.nettyrest.provider.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
+
+@Provider
+public class HystrixContextFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+	@Override
+	public void filter(final ContainerRequestContext requestContext) throws IOException {
+		HystrixRequestContext.initializeContext();
+	}
+
+	@Override
+	public void filter(final ContainerRequestContext requestContext,
+			ContainerResponseContext responseContext) throws IOException {
+		final HystrixRequestContext context = HystrixRequestContext.getContextForCurrentThread();
+		if (context != null) {
+			context.shutdown();
+		}
+	}
+
+}
