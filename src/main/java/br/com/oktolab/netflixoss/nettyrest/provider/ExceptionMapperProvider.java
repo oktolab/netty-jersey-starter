4058c6e87dce25b28b16e915571967e3d6515062
arthur.pdr@gmail.com <arthur.pdr@gmail.com>
2015-11-11 15:57:02 -0200

    added Message

--- src/main/java/br/com/oktolab/netflixoss/nettyrest/provider/ExceptionMapperProvider.java
+++ src/main/java/br/com/oktolab/netflixoss/nettyrest/provider/ExceptionMapperProvider.java
@@ -8,27 +8,51 @@ import javax.ws.rs.ext.Provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+
+import br.com.oktolab.netflixoss.nettyrest.exception.BusinessException;
+
 @Provider
 public class ExceptionMapperProvider implements ExceptionMapper<Throwable> {
 	
+	private static final int UNPROCESSABLE_ENTITY = 422;
+	
 	private static final Logger LOG = LoggerFactory.getLogger(ExceptionMapperProvider.class);
+	
 	private static final String RESPONSE_ERROR_TYPE = "text/plain";
 	private static final String MSG_INTERNAL_SERVER_ERROR = "Internal server error.";
+	
+	private static final Gson gson = new Gson();
 	
 	@Override
 	public Response toResponse(final Throwable ex) {
 		final Throwable realCause = getRealCause(ex);
 		LOG.debug(MSG_INTERNAL_SERVER_ERROR, realCause);
+		final int status = buildStatusCode(realCause);
+		final String message = realCause.getMessage();
+		return Response.status(status).entity(gson.toJson(new Message(message)))
+					.type(RESPONSE_ERROR_TYPE).build();
+	}
+
+	private int buildStatusCode(final Throwable realCause) {
 		if (realCause instanceof NotAuthorizedException) {
-			return Response.status(401).entity(realCause.getMessage()).type(RESPONSE_ERROR_TYPE)
-					.build();
+			return Response.Status.UNAUTHORIZED.getStatusCode();
+		} else if (realCause instanceof BusinessException) {
+			return UNPROCESSABLE_ENTITY;
 		}
-		return Response.status(500).entity(realCause.getMessage()).type(RESPONSE_ERROR_TYPE)
-				.build();
+		return Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
 	}
 
 	private Throwable getRealCause(Throwable ex) {
 		return ex.getCause() != null ? getRealCause(ex.getCause()) : ex;
+	}
+	
+	class Message {
+		String message;
+		
+		Message(String message) {
+			this.message = message;
+		}
 	}
 
 }
