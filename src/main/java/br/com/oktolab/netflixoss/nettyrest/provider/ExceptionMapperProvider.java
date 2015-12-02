21dd72fc4cac16c3479894343f12c4142ef0cecb
arthur.pdr@gmail.com <arthur.pdr@gmail.com>
2015-12-02 19:22:13 -0200

    fix response type, add tratamentos BusinessException e log

--- src/main/java/br/com/oktolab/netflixoss/nettyrest/provider/ExceptionMapperProvider.java
+++ src/main/java/br/com/oktolab/netflixoss/nettyrest/provider/ExceptionMapperProvider.java
@@ -1,6 +1,7 @@
 package br.com.oktolab.netflixoss.nettyrest.provider;
 
 import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -16,22 +17,38 @@ import br.com.oktolab.netflixoss.nettyrest.exception.BusinessException;
 public class ExceptionMapperProvider implements ExceptionMapper<Throwable> {
 	
 	private static final int UNPROCESSABLE_ENTITY = 422;
-	
 	private static final Logger LOG = LoggerFactory.getLogger(ExceptionMapperProvider.class);
-	
-	private static final String RESPONSE_ERROR_TYPE = "text/plain";
-	private static final String MSG_INTERNAL_SERVER_ERROR = "Internal server error.";
-	
+	private static final String MSG_INTERNAL_SERVER_ERROR = "Internal server error. Message: '%s'";
 	private static final Gson gson = new Gson();
 	
 	@Override
 	public Response toResponse(final Throwable ex) {
 		final Throwable realCause = getRealCause(ex);
-		LOG.debug(MSG_INTERNAL_SERVER_ERROR, realCause);
 		final int status = buildStatusCode(realCause);
-		final String message = realCause.getMessage();
-		return Response.status(status).entity(gson.toJson(new Message(message)))
-					.type(RESPONSE_ERROR_TYPE).build();
+		final String message = buildMessage(realCause);
+		logByCause(realCause, message);
+		return Response.status(status)
+						.entity(gson.toJson(new Message(message)))
+						.type(MediaType.APPLICATION_JSON).build();
+	}
+
+	private void logByCause(final Throwable realCause, String message) {
+		if (realCause instanceof BusinessException
+				|| realCause instanceof NotAuthorizedException) {
+			LOG.debug(String.format(MSG_INTERNAL_SERVER_ERROR, message), realCause);
+		} else {
+			LOG.error(String.format(MSG_INTERNAL_SERVER_ERROR, message), realCause);
+		}
+	}
+
+	private String buildMessage(final Throwable realCause) {
+		if (realCause == null) {
+			return null;
+		}
+//		if (realCause instanceof BusinessException) {
+			// TODO
+//		}
+		return realCause.getMessage();
 	}
 
 	private int buildStatusCode(final Throwable realCause) {
@@ -44,7 +61,15 @@ public class ExceptionMapperProvider implements ExceptionMapper<Throwable> {
 	}
 
 	private Throwable getRealCause(Throwable ex) {
-		return ex.getCause() != null ? getRealCause(ex.getCause()) : ex;
+		Throwable parentEx = ex.getCause();
+		if (parentEx != null) {
+			if (ex == parentEx) {
+				return ex;
+			}
+			return getRealCause(parentEx);
+		}
+		return ex;
+//		return ex.getCause() != null ? getRealCause(ex.getCause()) : ex;
 	}
 	
 	class Message {
